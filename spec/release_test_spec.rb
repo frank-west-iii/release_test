9c3c48fa011162badec6cfa0c409d581fea99d6d
Frank West <frank.west.iii@gmail.com>
2018-10-24 11:50:32 -0700

    Initial generation

---
+++ spec/release_test_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe ReleaseTest do
+  it "has a version number" do
+    expect(ReleaseTest::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
